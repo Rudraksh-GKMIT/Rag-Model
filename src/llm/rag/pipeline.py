12934c7228e7443fc12efa4ae7a4e4f72cf88f36
rudraksh-GKMIT <rudraksh@gkmit.co>
2026-02-04 17:58:54 +0530

    feat: add pipeline code

---
+++ src/llm/rag/pipeline.py
@@ -0,0 +1,16 @@
+from src.llm.rag.rag_agent.agent import RagAgent
+import warnings
+
+warnings.filterwarnings("ignore", category=ResourceWarning)
+
+def rag_model(chat_history,user_input):
+    agent = RagAgent()
+    try:
+        response  = agent.run(
+            chat_history=chat_history, user_message=user_input
+        )
+        # print(_)
+        return response
+
+    except Exception as e:
+        print(f"\n Error: {e}\n")
